aea0fe1120238e5e11db443065875da9d359a50f
João Tadiello - Muamba <tradertadiello@gmail.com>
2024-02-21 21:03:56 -0300

    feat: add debug command and text sizing

--- client.lua
+++ client.lua
@@ -1,21 +1,21 @@
 local toogle = false
-RegisterCommand('cl',function()
+
+RegisterCommand('cl', function()
     toogle = not toogle
     if toogle then
         Citizen.SetTimeout(15000, function()
             toogle = false
         end)
     end
-end,false)
+end, false)
 
 Citizen.CreateThread(function()
     while true do
         local idle = 1000
-        print(json.encode(GlobalState.cl))
         if toogle then
             idle = 1
-            for k,v in pairs(GlobalState.cl) do
-                DrawText3Ds(v.cds.x, v.cds.y, v.cds.z, v.text, 350)
+            for k, v in pairs(GlobalState.cl) do
+                DrawText3Ds(v.cds.x, v.cds.y, v.cds.z, v.text, 300)
             end
         end
         Wait(idle)
@@ -23,14 +23,14 @@ Citizen.CreateThread(function()
 end)
 
 function DrawText3Ds(x, y, z, text, size)
-	local onScreen, _x, _y = World3dToScreen2d(x, y, z)
-	SetTextFont(4)
-	SetTextScale(0.35, 0.35)
-	SetTextColour(255, 255, 255, 255)
-	SetTextEntry("STRING")
-	SetTextCentre(1)
-	AddTextComponentString(text)
-	DrawText(_x, _y)
-	local factor = (string.len(text)) / size
-	DrawRect(_x, _y + 0.0125, 0.01 + factor, 0.03, 0, 0, 0, 80)
+    local onScreen, _x, _y = World3dToScreen2d(x, y, z)
+    SetTextFont(4)
+    SetTextScale(0.5, 0.5)
+    SetTextColour(255, 255, 255, 255)
+    SetTextEntry("STRING")
+    SetTextCentre(1)
+    AddTextComponentString(text)
+    DrawText(_x, _y)
+    local factor = (string.len(text)) / size
+    DrawRect(_x, _y + 0.0125, 0.01 + factor, 0.05, 0, 0, 0, 255)
 end
--- server.lua
+++ server.lua
@@ -12,14 +12,32 @@ AddEventHandler("playerDropped", function(reason)
     local ped = GetPlayerPed(source)
     local cds = GetEntityCoords(ped)
     local temp = GlobalState.cl
-    temp[userId] = { cds = cds, text = '~w~[' .. userId .. "]~w~:~g~" .. reason }
+    temp[userId] = { cds = cds, text = '~r~ID:[' .. userId .. "]  ~g~[MOTIVO]:" .. reason}
     GlobalState.cl = temp
 
     Citizen.SetTimeout(15000, function()
         if GlobalState.cl[userId] then
             local temp = GlobalState.cl
             temp[userId] = nil
-            print('limpou')
         end
     end)
 end)
+
+--- [ DEBUG COMMAND ]
+-- RegisterCommand('test',function(source)
+--     local source = source
+--     local userId = BASE == 'vrpex' and vRP.getUserId(source) or vRP.Passport(source)
+--     local ped = GetPlayerPed(source)
+--     local cds = GetEntityCoords(ped)
+--     local temp = GlobalState.cl
+--     temp[userId] = { cds = cds, text = '~r~ID:[' .. userId .. "]  ~g~[MOTIVO]:" .. 'DEBUG' }
+--     GlobalState.cl = temp
+
+--     Citizen.SetTimeout(15000, function()
+--         if GlobalState.cl[userId] then
+--             local temp = GlobalState.cl
+--             temp[userId] = nil
+--             print('limpou')
+--         end
+--     end)
+-- end,false)
